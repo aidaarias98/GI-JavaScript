a3f64cf857cf1efa0a630562101674c313757559
R2H <r2h@AdminisatorsMBP.attlocal.net>
2022-09-18 16:59:26 -0400

    Very Easy Challenge

---
+++ veryeasy.js
@@ -0,0 +1,13 @@
+// //very easy:
+
+const num1 =3000;
+const num2 = 2;
+
+ console.log(num1-num2);
+ console.log(`The difference between ${num1} and ${num2} is ${num1-num2}.`);
+
+//  //very easy if I did it using an alert prompt:
+// alert ("Hi,insert two numbers");
+// var num1=prompt("Insert your first number");
+// var num2=prompt("Insert your second number");
+// console.log(`The difference between ${num1} and ${num2} is ${num1-num2}.`);
